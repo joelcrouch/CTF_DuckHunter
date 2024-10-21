a0728c5f9643b0c36027e5425d29837b1f7ebd09
Joel <joelcrouch@gmail.com>
2024-10-21 11:49:12 -0700

    add move,moverandomly,pickupflag,movetoflage

--- src/Duck.java
+++ src/Duck.java
@@ -7,10 +7,24 @@ import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
+
 public abstract class Duck {
     protected RobotController rc;
     protected Random rng;
     protected int turnCount = 0;
+
+
+    /** Array containing all the possible movement directions. */
+    static final Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+    };
 
     public Duck(RobotController rc) {
         this.rc = rc;
@@ -20,19 +34,58 @@ public abstract class Duck {
     // Abstract method that must be implemented by subclasses
     public abstract void run() throws GameActionException;
 
-    // Common movement or helper/escape flag/pickupflags methods can be added here
-    // All the stuff in common
+
+
+    // Common movement behavior
     protected void moveRandomly() throws GameActionException {
-        Direction[] directions = Direction.values();
         Direction randomDir = directions[rng.nextInt(directions.length)];
         if (rc.canMove(randomDir)) {
             rc.move(randomDir);
         }
     }
 
+    // General-purpose movement method
     public void move(Direction dir) throws GameActionException {
         if (rc.canMove(dir)) {
             rc.move(dir);
         }
     }
+
+    // Sense nearby enemies
+    protected RobotInfo[] senseEnemies() throws GameActionException {
+        return rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+    }
+
+    // pickup or sensing a flag
+    protected void findAndPickupFlag() throws GameActionException {
+        if (rc.canPickupFlag(rc.getLocation())) {
+            rc.pickupFlag(rc.getLocation());
+            rc.setIndicatorString("Picked up a flag!");
+        }
+    }
+
+    // General method to find flag and move toward it
+    protected void moveToFlag() throws GameActionException {
+        if (rc.hasFlag()) {
+            MapLocation[] spawnLocs = rc.getAllySpawnLocations();
+            if (spawnLocs.length > 0) {
+                MapLocation nearestSpawn = spawnLocs[0];  // Choose nearest spawn location
+                Direction toSpawn = rc.getLocation().directionTo(nearestSpawn);
+                move(toSpawn);
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
